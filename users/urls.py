0c4a4a3a79c133a3246bacf7923206bf7afc38dd
Вероника <nika@Ivans-MacBook-Pro.local>
2020-06-07 12:14:21 +0300

    Passed 6 tests

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signup/", views.SignUp.as_view(), name="signup")
+    path("signup/", views.SignUp.as_view(), name="signup"),
+    path("new/", views.SignUp.as_view(), name = 'new_post')
 ]
